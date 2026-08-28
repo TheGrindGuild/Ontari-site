24a961e0f9b52988796451c0e611fee517294251
$LFGRIND <126117561+TheGrindGuild@users.noreply.github.com>
2026-08-28 09:10:57 -0400

    Add Cast a Stone section to page.

--- ontari-site/src/app/page.tsx
+++ ontari-site/src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Hero } from "@/components/sections/Hero";
 import { Story } from "@/components/sections/Story";
 import { MemeSection } from "@/components/sections/MemeSection";
+import { CastStoneSection } from "@/components/cast-stone/CastStoneSection";
 import { TokenSection } from "@/components/sections/TokenSection";
 import { SocialSection } from "@/components/sections/SocialSection";
 import { Footer } from "@/components/sections/Footer";
@@ -11,6 +12,7 @@ export default function Home() {
       <Hero />
       <Story />
       <MemeSection />
+      <CastStoneSection />
       <TokenSection />
       <SocialSection />
       <Footer />
